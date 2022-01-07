31c17b76920f84cec06191ed68c571e4f097025e
Roman <ram219219@gmail.com>
2022-01-07 22:04:49 +0200

    Added registration v1.0

--- src/main/java/com/rb/estore/controller/AuthenticationControlller.java
+++ src/main/java/com/rb/estore/controller/AuthenticationControlller.java
@@ -1,6 +1,7 @@
 package com.rb.estore.controllers;
 
 import com.rb.estore.model.view.RegisterUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,6 +10,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AuthenticationControlller {
+
+    @Autowired
+    AuthenticationService authenticationService;
+
     @RequestMapping(value = "/registration", method = RequestMethod.GET)
     public String registerShow(Model model) {
         model.addAttribute("ruser", new RegisterUser());
@@ -20,6 +25,8 @@ public class AuthenticationControlller {
 
         System.out.println("User name: ");
         System.out.println(registerUser.getName());
+
+        this.authenticationService.register(registerUser);
 
         return "redirect:/main";
     }
